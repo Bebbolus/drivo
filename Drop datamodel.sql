45095bb8b61d784cd7e4c33ccb403047c38d843a
alessiofiore <alessiofiore@gmail.com>
2014-07-08 15:04:33 +0200

    drop datamodel script

---
+++ Drop datamodel.sql
@@ -0,0 +1,18 @@
+delete from migrations;
+
+DROP TABLE vehicle_abilitations;
+DROP TABLE school_calendars;
+DROP TABLE instructor_abilitations;
+DROP TABLE instructor_calendars;
+DROP TABLE vehicle_calendars;
+DROP TABLE status_calendars;
+drop table prices;
+drop table reservations;
+drop table instructors;
+drop table licences;
+drop table customers;
+drop table users;
+drop table transfers;
+drop table vehicles;
+drop table addresses;
+drop table schools;
